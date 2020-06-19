bce2f9456d42d3f5a872303fa9082e6caf49d3cc
rumothy <rumothy@yahoo.com>
2020-06-18 22:14:09 -0400

    Route query.

---
+++ controllers/searchController.js
@@ -0,0 +1,71 @@
+const fetch = require("node-fetch");
+const keys = require("../private/token");
+
+module.exports = {
+  search: function (req, res) {
+    const key = req.params.key;
+    console.log("key:", key);
+    let query = buildQuery(key, "", "first: 10");
+    executeQuery(query)
+      .then((r) => r.json())
+      .then((data) => {
+        console.log("data returned:", data);
+        let dataData = data.data;
+        let search = dataData.search;
+        res.send(search);
+      });
+  },
+};
+
+function executeQuery(query) {
+  return fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `bearer ${keys.rumsToken}`,
+    },
+    body: query,
+  });
+}
+
+function buildQuery(key, before, after) {
+  let query = `{ 
+        search(query: "${key}", type: REPOSITORY, ${before} ${after}) {
+            edges {
+                node {
+                    ... on Repository {
+                    id
+                    name
+                    stargazers {
+                        totalCount
+                    }
+                    description
+                    url
+                        owner {
+                            id
+                            ... on User {
+                                id
+                                name
+                                avatarUrl
+                                followers {
+                                    totalCount
+                                }
+                            }
+                        }
+                    }
+                }
+                cursor
+            }
+            repositoryCount
+            pageInfo {
+              endCursor
+              hasNextPage
+              hasPreviousPage
+              startCursor  
+            }
+        }  
+    }`;
+
+  return JSON.stringify({ query });
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const searchController = require("./controllers/searchController");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,11 +11,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-app.get("/api/search/:key", function (req, res) {
-  console.log(req.params.key);
-  res.send({ response: "thanks" });
-});
+app.get("/api/search/:key", searchController.search);
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
